5a005249aaba5b5265fdf771467379e9fe0e6535
jaromir.sida <jaromir.sida@postgrad.manchester.ac.uk>
2020-08-29 10:12:16 +0200

    new scripts for benchmark model and price model

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -9,6 +9,9 @@ import GetOldTweets3 as got
 import pandas as pd
 import os
 import time
+import glob
+import shutil
+from datetime import timedelta, date
 
 def to_dataframe(tweets):
     """Coverts Tweet object to Pandas DataFrame. 
@@ -60,62 +63,116 @@ def to_csv(df, path, file_name):
     else:
         print("File not saved.")
         
-def retrieve_tweets(search_query, start_date, end_date, path, max_limit, language,
+def daterange(start_date, end_date):
+    
+    """ produce a list of dates within an interval"""
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+          
+def check_file_existence(path, file_prefix):
+    """ verifies an exstince of csv files in given directory"""
+    files = glob.glob(path+'\\' + file_prefix + '*.csv')
+    
+    if len(files) > 0 :
+        return True
+    else:
+        return False
+
+def archive_files(path, file_prefix):
+    """ archives all files with certain prefix to a new archvie folder """
+    if check_file_existence(path, file_prefix):
+        
+        cur_time = time.localtime()
+        
+        year = str(cur_time.tm_year)
+        month = str(cur_time.tm_mon).zfill(2)
+        day = str(cur_time.tm_mday).zfill(2)
+        hour = str(cur_time.tm_hour).zfill(2)
+        minute = str(cur_time.tm_min).zfill(2)
+        #sec = str(time.localtime().tm_sec).zfill(2)
+        
+        archive_dir_name = path+"\\00_Archive\\" + year+month+day+"_"+hour+minute
+        
+        os.mkdir(archive_dir_name)
+
+        files = glob.glob(path+'\\' + file_prefix + '*.csv')
+        
+        for file in files:
+            file_name = os.path.basename(file)
+            shutil.move(file, archive_dir_name+"\\"+file_name)
+
+        
+def retrieve_tweets(search_query, file_prefix, start_date, end_date, path, max_limit, language,
                     near):
     """ Dowload tweets and saves them to a csv file"""
     
-    # search criteria
-    tweetCriteria = got.manager.TweetCriteria().setQuerySearch(search_query)\
-                                               .setSince(start_date)\
-                                               .setUntil(end_date)\
-                                               .setEmoji("unicode")\
-                                               .setMaxTweets(max_limit)\
-                                               .setLang(language)\
-                                               .setNear(near)
-    # get tweets
-    tweet = got.manager.TweetManager.getTweets(tweetCriteria)
+    # archive existing files
+    archive_files(path,"$")
     
-    # store downloaded tweets into a dataframe
-    df = to_dataframe(tweet)
-    print(df.shape)
-    length = df.shape[0]
-    # save dataframe into a csv file
-    to_csv(df, path, search_query+"_"+start_date+"_"+end_date)
+    batch = 0
     
-    del df
+
+    for single_date in daterange(start_date, end_date):
+        
+        if batch == 0 or batch % 15 != 0:
+        
+            single_date_next = single_date + timedelta(1)
+            print(single_date, single_date_next)
+            
+            start = time.time()
+            # search criteria
+            tweetCriteria = got.manager.TweetCriteria().setQuerySearch(search_query)\
+                                                       .setSince(single_date.strftime("%Y-%m-%d"))\
+                                                       .setUntil(single_date_next.strftime("%Y-%m-%d"))\
+                                                       .setEmoji("unicode")\
+                                                       .setMaxTweets(max_limit)\
+                                                       .setLang(language)\
+                                                       .setNear(near)
+            # get tweets
+            tweet = got.manager.TweetManager.getTweets(tweetCriteria)
+        
+            # store downloaded tweets into a dataframe
+            df = to_dataframe(tweet)
+            length = df.shape[0]
+            # save dataframe into a csv file
+            
+            full_file_path = path + "\\" + search_query+"_"+start_date.strftime("%Y-%m-%d")+"_"+end_date.strftime("%Y-%m-%d")+".csv"
+            
+            
+            if batch == 0 :
+                df.to_csv(full_file_path)
+                batch += 1
+            else:
+                df.to_csv( full_file_path, mode='a', header=False)
+                batch += 1
+                
+            end = time.time()
+            total_time = end-start
+            print(f"{search_query} --- batch no: {batch} ---length : {length}", total_time)
+    
+            del df
+        else:
+            # every 14 batches sleep for 15 minutes to avoid hitting the max limit
+            print("going to sleep at:", time.asctime( time.localtime(time.time())))
+            time.sleep(15*60)
+            batch += 1
+            
     return length
 
-time_stats = pd.DataFrame(columns=['query','start_date','end_date','count','time'])
 
 trg_file_path = r'c:\Users\jaromir\OneDrive\UoM\100_Disertation\02_SrcData'
-start_date = "2019-03-19"
-end_date = "2020-06-19"
+start_date = date(2020,3,30)
+end_date = date(2020,6,30)
+#2020-03-01
+#2020-06-30
 language = 'en'
 near = '840'
-tickers = ['#KO', '#XOM','#TSLA','#JPM','#DIS']
-max_limit = 10000
+tickers = ['$DAL']
+max_limit = 1000
 
 for idx, ticker in enumerate(tickers):
     start = time.time()
-    tweet_count = retrieve_tweets(ticker, start_date, end_date, trg_file_path, max_limit,
+    tweet_count = retrieve_tweets(ticker, "$", start_date, end_date, trg_file_path, max_limit,
                                   language, near)
     end = time.time()
     total_time = end-start
-
-    stats = pd.DataFrame({'query':ticker,'start_date':start_date,'end_date':end_date,
-                      'count': tweet_count, "time":total_time}, index=[idx])
-
-#    time_stats = pd.concat([time_stats,stats], axis = 0 )
-    pd.concat([time_stats, stats], axis=0)
-
-#if __name__ == "__main__":
-#
-#end = time.time()
-#print(end-start)
-    
-# XOM - energy
-# TSLA - automotive
-# JPM - banking 
-# DIS - media
-# KO - food
-    
